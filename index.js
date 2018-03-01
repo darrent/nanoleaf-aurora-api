ee7c59f9a7d46c1f4e3526bd44b1b9433bdf4bf6
Ansel Zandegran <anselzandegran@Ansels-MacBook-Pro.local>
2018-03-01 22:37:22 +0100

    Add RGB and HSV Support

--- index.js
+++ index.js
@@ -108,6 +108,46 @@ var AuroraApi = module.exports = function (options) {
   };
 };
 
+this.rgb2hsv = function  () {
+  var rr, gg, bb,
+  r = arguments[0] / 255,
+  g = arguments[1] / 255,
+  b = arguments[2] / 255,
+  h, s,
+  v = Math.max(r, g, b),
+  diff = v - Math.min(r, g, b),
+  diffc = function(c){
+    return (v - c) / 6 / diff + 1 / 2;
+  };
+
+  if (diff == 0) {
+    h = s = 0;
+  } else {
+    s = diff / v;
+    rr = diffc(r);
+    gg = diffc(g);
+    bb = diffc(b);
+
+    if (r === v) {
+      h = bb - gg;
+    }else if (g === v) {
+      h = (1 / 3) + rr - bb;
+    }else if (b === v) {
+      h = (2 / 3) + gg - rr;
+    }
+    if (h < 0) {
+      h += 1;
+    }else if (h > 1) {
+      h -= 1;
+    }
+  }
+  return {
+    h: Math.round(h * 360),
+    s: Math.round(s * 100),
+    v: Math.round(v * 100)
+  };
+};
+
 AuroraApi.prototype.getInfo = function () {
   const requestOptions = this.makeGetRequestOptions('/');
 
@@ -187,6 +227,17 @@ AuroraApi.prototype.setHSV = function (h,s,v) {
     sat: s,
     hue: h,
     brightness: v
+  });
+
+  return this.doRequest(requestOptions);
+};
+
+AuroraApi.prototype.setRGB = function (r,g,b) {
+  var hsv = this.rgb2hsv(r,g,b);
+  const requestOptions = this.makePutRequest('/state', {
+    sat: hsv.s,
+    hue: hsv.h,
+    brightness: hsv.v
   });
 
   return this.doRequest(requestOptions);
